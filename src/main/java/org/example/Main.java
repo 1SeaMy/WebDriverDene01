a658b7f6c496a685e1da481a42b8a6579be0d71d
Deniz Albayrak <92841021+1SeaMy@users.noreply.github.com>
2023-03-26 04:02:14 +0300

    First Change 03

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Degisiklik 01");
+        System.out.println("Degisiklik 02");
+        System.out.println("Degisiklik 03");
     }
 }
